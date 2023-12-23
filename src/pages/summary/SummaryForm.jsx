e9aafbcd6b2547bc205cad2538fd2d9c4fba9cea
eyo25 <qutuzm@naver.com>
2023-12-23 23:52:20 +0900

    test: popover 테스트 코드 작성

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,13 +1,24 @@
-import { useState } from "react";
-import { Button, Form, OverlayTrigger } from "react-bootstrap";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
 
-function SummaryForm() {
-  const [isChecked, setIsChecked] = useState(false);
+export default function SummaryForm() {
+  const [tcChecked, setTcChecked] = useState(false);
+
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
 
   const checkboxLabel = (
     <span>
       I agree to
-      <span style={{ color: "blue" }}> Terms and Conditions</span>
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}> Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   );
 
@@ -16,16 +27,14 @@ function SummaryForm() {
       <Form.Group controlId="terms-and-conditions">
         <Form.Check
           type="checkbox"
-          checked={isChecked}
-          onChange={(e) => setIsChecked(e.target.checked)}
+          checked={tcChecked}
+          onChange={(e) => setTcChecked(e.target.checked)}
           label={checkboxLabel}
         />
       </Form.Group>
-      <Button variant="primary" type="submit" disabled={!isChecked}>
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
         Confirm order
       </Button>
     </Form>
   );
 }
-
-export default SummaryForm;
--- src/tests/Summary.test.jsx
+++ src/tests/Summary.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import { expect } from "vitest";
+import { expect, test } from "vitest";
 import SummaryForm from "../pages/summary/SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 test("시작 상태 테스트", () => {
   // render
@@ -8,7 +9,7 @@ test("시작 상태 테스트", () => {
 
   // find the button
   const checkbox = screen.getByRole("checkbox", {
-    id: "terms-and-conditions",
+    name: /terms and conditions/i,
   });
 
   // 체크안되었는지 테스트
@@ -19,23 +20,48 @@ test("시작 상태 테스트", () => {
   expect(confirmButton).toBeDisabled();
 });
 
-test("클릭했을때 버튼 비활성화 테스트", () => {
+test("클릭했을때 버튼 비활성화 테스트", async () => {
+  const user = userEvent.setup();
+
   // render
   render(<SummaryForm />);
 
   // find the button and confirm button
   const checkbox = screen.getByRole("checkbox", {
-    id: "terms-and-conditions",
+    name: /terms and conditions/i,
   });
   const confirmButton = screen.getByRole("button", { name: "Confirm order" });
 
   // 클릭 후 체크 되었는지와 버튼 사용 가능한지 테스트
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(checkbox).toBeChecked();
   expect(confirmButton).toBeEnabled();
 
   // 다시 클릭 후 체크가 해제 되고 버튼 사용 비활성화 테스트
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(checkbox).not.toBeChecked();
   expect(confirmButton).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+
+  // 팝오버는 시작하면 안보이는 상태
+  const nullPopover = screen.queryAllByText(
+    /No ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // 팝오버를 마우스 호버하면 보여야한다.
+  const termAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termAndConditions);
+  const popover = screen.getByText(/No ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  // 팝오버는 마우스가 벗어나면 안보여야한다.
+  await user.unhover(termAndConditions);
+  expect(nullPopover).not.toBeInTheDocument();
 });
