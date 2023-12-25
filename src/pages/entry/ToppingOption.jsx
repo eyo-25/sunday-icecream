65de7dc0112bf425cb0bfd90c9d045df161a3e67
eyo25 <qutuzm@naver.com>
2023-12-26 01:03:46 +0900

    feat: toppingOption  페이지 구현

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -6,7 +6,7 @@ function ToppingOption({ name, imagePath }) {
       <img
         style={{ width: "75%" }}
         src={`http://localhost:3030/${imagePath}`}
-        alt={`${name} toppings`}
+        alt={`${name} topping`}
       />
     </Col>
   );
--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -5,7 +5,7 @@ import { expect } from "vitest";
 test("서버에서 가져온 각각의 스쿱옵션이 이미지로 표현되는가", async () => {
   render(<Options optionType="scoops" />);
   // 이미지를 찾습니다.
-  const scoopsImage = await screen.findAllByRole("img", { name: /scoops$/i });
+  const scoopsImage = await screen.findAllByRole("img", { name: /scoop$/i });
   expect(scoopsImage).toHaveLength(2);
   // 이미지 대체 텍스트 확인
   const altTexts = scoopsImage.map((item) => item.alt);
@@ -13,13 +13,15 @@ test("서버에서 가져온 각각의 스쿱옵션이 이미지로 표현되는
 });
 
 test("서버에서 가져온 각각의 토핑옵션이 이미지로 표현되는가", async () => {
-  render(<Options optionType="topping" />);
+  render(<Options optionType="toppings" />);
 
+  //이미지를 찾고 3개를 msw에서 return하는지 테스트
   const toppingImage = await screen.findAllByRole("img", {
-    name: /toppings$/i,
+    name: /topping$/i,
   });
+  expect(toppingImage).toHaveLength(3);
 
-  // 이미지 대체 텍스트 확인
+  // 이미지 대체 텍스트 테스트로 서버로 부터 데이터가 잘 왔는지 테스트
   const altTexts = toppingImage.map((item) => item.alt);
   expect(altTexts).toEqual([
     "Cherries topping",
